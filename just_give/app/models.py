85602673d8b2384ad7716ec9b25d9cd4163e8d0c
Djordje J <jovanovic.djo@gmail.com>
2024-08-06 21:48:49 +0200

    add initial models without relationships

--- just_give/app/models.py
+++ just_give/app/models.py
@@ -23,6 +23,9 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    middle_name = Column(String, nullable=True)
+    last_name = Column(String, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     date_registered = Column(
@@ -35,4 +38,61 @@ class User(Base):
             TIMESTAMP(timezone=True), nullable=True, server_default=None
         )
 
+
+class City(Base):
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    city_name = Column(String, nullable=False, unique=True)
+
+
+class Country(Base):
+    __tablename__ = "countries"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    country_name = Column(String, nullable=False, unique=True)
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    category_name = Column(String, nullable=False, unique=True)
+
+
+class Subcategory(Base):
+    __tablename__ = "subcategories"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    subcategory_name = Column(String, nullable=False, unique=True)
+
+
+class Type(Base):
+    __tablename__ = "types"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    type_name = Column(String, nullable=False, unique=True)
+
+
+class GiveOut(Base):
+    __tablename__ = "give_outs"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    published_date = Column(
+        TIMESTAMP(timezone=True), nullable=True, server_default=text("now()")
+    )
+    due_date = Column(
+        TIMESTAMP(timezone=True), nullable=True, server_default=None
+    )
+    
+
+class Interaction(Base):
+    __tablename__ = "interactions"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    give_out_id = Column(Integer, ForeignKey('give_outs.id'), nullable=False)
+    interaction_time = Column(
+        TIMESTAMP(timezone=True), nullable=True, server_default=text("now()")
+    )
 
